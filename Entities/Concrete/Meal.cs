c55077d145acfb232da178b07bf2c888a8b9317c
Ayça Akdemir <mabeaaa@gmail.com>
2021-05-20 22:59:56 +0300

    20.05

---
+++ Entities/Concrete/Meal.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Meal:IEntity
+    {
+        public int Id { get; set; }
+        public int MenuId { get; set; }
+        public string MealName { get; set; }
+        public int MealImageId { get; set; }
+        public string MealPrice { get; set; }
+    }
+}
---
+++ Entities/Concrete/Menu.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+   public class Menu :IEntity
+    {
+        public int Id { get; set; }
+        public string MenuName { get; set; }
+        public string MenuDescription { get; set; }
+        public int MenuImageId { get; set; }
+    }
+}
